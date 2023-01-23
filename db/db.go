949506f330e3fe22fd957b1b69a81442765cecab
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2023-01-23 00:06:42 -0500

    working write/read

--- db/db.go
+++ db/db.go
@@ -24,16 +24,16 @@ func newDataKv(key, value string) kv {
 
 	// Create a data payload with the following byte signature:
 	// key header:   8 bytes indicating length in bytes of key data
-	// key data:     variable length
 	// value header: 8 bytes indicating length in bytes of value data
+	// key data:     variable length
 	// value data:   variable length
 	data := make([]byte, 0, 128+kl+vl)
 	buffer := make([]byte, 8)
-	binary.LittleEndian.PutUint64(buffer, kl)
+	binary.BigEndian.PutUint64(buffer, kl)
+	data = append(data, buffer...)
+	binary.BigEndian.PutUint64(buffer, vl)
 	data = append(data, buffer...)
 	data = append(data, kbytes...)
-	binary.LittleEndian.PutUint64(buffer, vl)
-	data = append(data, buffer...)
 	data = append(data, vbytes...)
 
 	return kv{key, value, data}
@@ -54,7 +54,7 @@ type memtable struct {
 }
 
 func newMemtable() memtable {
-	return memtable{0, 256, *btree.NewG(2, less)}
+	return memtable{0, 64, *btree.NewG(2, less)}
 }
 
 func (m *memtable) add(key, value string) {
@@ -83,6 +83,26 @@ func (m memtable) serialize() []byte {
 		return true
 	})
 	return bytes
+}
+
+func getSerialized(key string, logfile []byte) (string, bool) {
+	for offset := 0; offset < len(logfile); {
+		klo := uint64(offset) // key-length field offset
+		vlo := klo + 8        // value-length field offset
+		kdo := vlo + 8        // key data field offset
+
+		keyLen := binary.BigEndian.Uint64(logfile[klo:vlo])
+		valLen := binary.BigEndian.Uint64(logfile[vlo:kdo])
+		vdo := kdo + keyLen // value data field offset
+		if keyLen == uint64(len(key)) {
+			loggedKey := string(logfile[kdo:vdo])
+			if key == loggedKey {
+				return string(logfile[vdo : vdo+valLen]), true
+			}
+		}
+		offset = int(vdo + valLen)
+	}
+	return "", false
 }
 
 type Db struct {
@@ -117,6 +137,21 @@ func (db Db) Get(key string) (string, bool) {
 	if ok {
 		return value, true
 	} else {
+		for i := db.head - 1; ; i-- {
+			path := db.logFilePath(i)
+			log.Printf("Searching for %v in %v", key, path)
+			bytes, err := os.ReadFile(path)
+			if err != nil {
+				log.Fatalf("Failed to open log file %v: %v\n", path, err)
+			}
+			value, ok := getSerialized(key, bytes)
+			if ok {
+				return value, true
+			}
+			if i == 0 {
+				break
+			}
+		}
 		return "", false
 	}
 }
@@ -126,7 +161,7 @@ func (db *Db) flush() error {
 	os.MkdirAll(db.datadir, 0700)
 
 	f, err := os.OpenFile(
-		filepath.Join(db.datadir, fmt.Sprint(db.head, ".log")),
+		db.logFilePath(db.head),
 		os.O_CREATE|os.O_WRONLY,
 		0600)
 	if err != nil {
@@ -141,5 +176,10 @@ func (db *Db) flush() error {
 
 	log.Printf("Wrote %v bytes to %v", bytes, f.Name())
 	db.head += 1
+	db.memtable = newMemtable()
 	return nil
+}
+
+func (db Db) logFilePath(n uint) string {
+	return filepath.Join(db.datadir, fmt.Sprint(n, ".log"))
 }
